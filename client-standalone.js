b878cff6ca6cad3b14b7dcf16125a84a8bcac70b
Renato Costa <mikitow@gmail.com>
2022-02-24 18:55:14 -0800

    Export Axios API client.

--- client-standalone.js
+++ client-standalone.js
@@ -1,2 +1,5 @@
+import apiClient from './client/utils/apiClient';
+
 export { P5Editor } from './client';
 export { P5Preview } from './client/modules/Preview/previewIndex';
+export const AxiosApiClient = apiClient;
